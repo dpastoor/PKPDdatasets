2153e88d212337f4dde0f101850201e0b97c30a8
Devin Pastoor <devin.pastoor@gmail.com>
2014-02-27 18:48:40 -0500

    add one cmpt single IV bolus PKPD dataset

--- R/PKPDdatasets.R
+++ R/PKPDdatasets.R
@@ -26,3 +26,41 @@ NULL
 #' @usage data(sd_oral_richpk)
 #' @format A data frame with 4300 rows and 9 variables
 NULL
+
+#' One-compartment single IV bolus PKPD
+#' 
+#' A dataset containing simulated PKPD data. A single IV bolus dose was given and rich pharmacokinetic
+#' and pharmacodynamic measurements were taken. Demographic data of Age, Weight, Gender, and Race were collected.
+#' 
+#' The model used is a one-compartment PK model with allometric model of weight on CL (exp 0.75) and volume (exp 1) 
+#' with direct PD effect (Emax model). 
+#' 
+#' \itemize{
+#'   \item ID. Numerical ID  (1-100)
+#'   \item TIME Time in hours (0-12)
+#'   \item COBS. Plasma concentration in 
+#'   \item EOBS. 
+#'   \item WEIGHT. Weight in kg
+#'   \item AGE. Age in years
+#'   \item DOSE. Three dose levels: 1, 10, and 100 mg
+#'   \item SEX. Male or Female gender identification
+#'   \item RACE. AfricanAmerican, Caucasian, Others, and Asian races
+#'   \item AMT. Amount of drug given, time dependent, in milligrams
+#' }
+#' @details 
+#' Parameters used to simulate data:
+#'   \tabular{lcr}{
+#' Parameter                    \tab Mean \tab Variance (BSV)\cr
+#' CL (L/hr)                    \tab 1    \tab 0.16 (40\%)\cr
+#' V (L)                        \tab 10   \tab 0.09 (30\%)\cr
+#' Residual error: Proportional \tab 0    \tab 10\%\cr
+#' Emax                         \tab 10   \tab sd = 1.4 units\cr
+#' EC50 (mg/L)                  \tab 2    \tab 0.09 (30\%)\cr
+#' Residual Error on Effect     \tab 0    \tab sd = 0.5 units
+#' }
+#' @docType data
+#' @keywords datasets
+#' @name sd_iv_rich_pkpd
+#' @usage data(sd_iv_rich_pkpd)
+#' @format A data frame with 480 rows and 10 variables
+NULL
