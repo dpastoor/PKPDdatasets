0d7ba353a04dad559fe8c577f3fea93512a9639f
Devin Pastoor <devin.pastoor@gmail.com>
2014-04-18 12:00:27 -0400

    add additional list_datasets function since it feels more natural than print_datasets

--- R/print_datasets.R
+++ R/print_datasets.R
@@ -3,3 +3,12 @@
 #' @examples
 #' print_datasets()
 print_datasets <- function()data(package = "PKPDdatasets")
+
+#' List datasets available in PKPDdatasets package
+#' @name list_datasets
+#' @export
+#' @examples
+#' list_datasets()
+list_datasets <- print_datasets
+
+
