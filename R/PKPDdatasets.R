bf30b505ee9d156fca1f1720658d8c1a9cc74211
Devin Pastoor <devin.pastoor@gmail.com>
2014-09-12 15:30:30 -0400

    add dapa_IV_oral

--- R/PKPDdatasets.R
+++ R/PKPDdatasets.R
@@ -65,3 +65,27 @@ NULL
 #' @usage data(sd_iv_rich_pkpd)
 #' @format A data frame with 480 rows and 10 variables
 NULL
+
+
+#' IV and oral pharmacokinetic data for daptomycin
+#' 
+#' A dataset containing simulated dapagliflozin PK.  A single IV dose followed by 3 escalating oral doses
+#' \itemize{
+#'   \item ID. Numerical ID  (1-24)
+#'   \item TIME. Nominal Time after first dose (hrs)
+#'   \item TAD. Time After Dose (hrs)
+#'   \item COBS. Observed Concentration (ug/L)
+#'   \item AMT_IV. IV dose amount when given (ug)
+#'   \item AMT_ORAL. Oral dose amount when given (ug)
+#'   \item OCC. Occasion, associated with each dosing event
+#'   \item AGE. Age (years)
+#'   \item WEIGHT Weight (kg)
+#'   \item GENDER. Gender flag (female=1, male=0)
+#'   \item FORMULATION Formulation associated with dose (IV or oral)
+#' }
+#' @docType data
+#' @keywords datasets
+#' @name dapa_IV_oral
+#' @usage data(dapa_IV_oral)
+#' @format A data frame with 1536 rows and 11 variables
+NULL
