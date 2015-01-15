2f016c73f6481175e256746ae43fab804e776cbd
Devin Pastoor <devin.pastoor@gmail.com>
2015-01-15 17:13:15 -0500

    add aht_trial1 for Mathangi (not correct documentation)

---
+++ R/continuous_datasets.R
@@ -0,0 +1,24 @@
+
+#' One-compartment pharmacokinetic data given single oral dose
+#' 
+#' A dataset containing dose, plasma concentration and time data, as well as 
+#'  demographic data of Age, Weight, Gender, and Race for each individual
+#' 
+#' \itemize{
+#'   \item ID. Numerical ID  (1--50)
+#'   \item AGE. Time in hours (0--24)
+#'   \item WEIGHT Amount of drug given, time dependent, in milligrams
+#'   \item SCREATININE Plasma concentration in mg/L
+#'   \item Age. Age in years
+#'   \item Weight. Weight in kg
+#'   \item Gender. Male or Female gender identification
+#'   \item Race. Ethnicity
+#'   \item Dose. Dose given to each individual in milligrams
+#' }
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name aht_trial1
+#' @usage data(aht_trial1)
+#' @format A data frame with 4300 rows and 9 variables
+NULL
