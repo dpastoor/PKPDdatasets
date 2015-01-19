cda4c32c63cc7d67a6b712c0f1c0b591bdf3e088
MathangiCTM <mgopalakrishnan@rx.umaryland.edu>
2015-01-18 22:09:50 -0500

    ahttrial_II data summary
    Description of data variables

---
+++ R/ahttrial_II.R
@@ -0,0 +1,36 @@
+
+#' Anti-hypertensive trial clinical endpoint data
+#' 
+#' AHT trial data set: Randomized, placebo controlled, Double-blind, parallel study;    								
+#'Subjects randomized to Placebo and AHT in the ratio 1:1; Dose of AHT studied : 5mg									
+#'Primary endpoint: Change from baseline blood pressure (systolic and diastolic) measured at 8 weeks.									
+#'Details of the Data Column: ID=Subject ID, Age: Age(years), Weight : Weight(Kg), SCreatinine: Serum Creatinine(mg/dl), 
+#'SerumALT: Serum Alanine aminotransferase(IU/L), DelDBP: Change from baseline diastolic blood pressure(mm Hg), 
+#'DELSBP: Change from baseline systolic blood pressure (mm Hg), TRT: Treatment, Dose: Dose (microgram,µg)									
+
+#' 
+#' \itemize{
+#'   \item ID. Numerical ID  (1--100)
+#'   \item AGE. Age in Years 
+#'   \item WEIGHT Weight in Kg
+#'   \item SCREATININE. Serum creatinine in mg/dl
+#'   \item SERUMALT. Serum Alanine Aminotransferase  in IU/L
+#'   \item GENDER. Male or Female gender identification
+#'   \item RACE. Ethnicity
+#'   \item DELDBP. Change from baseline in Diastolic blood pressure, mmHg 
+#'   \item DELSBP. Change from baseline in Systolic blood pressure, mmHg 
+#'   \item TRT. Treatment indicator: Placebo and AHT group 
+#'   \item DOSE. Dose of AHT in ug 
+#' }
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name ahttrial_II
+#' @usage data(ahttrial_II)
+#' @format A data frame with 100 rows and 11 variables
+NULL
+
+
+
+
+								
